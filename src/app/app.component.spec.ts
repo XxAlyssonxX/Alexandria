aa8e51428d22ff277ea243da518ea45225b714a2
XxAlyssonxX <132359941+XxAlyssonxX@users.noreply.github.com>
2024-11-07 10:52:24 -0300

    jonas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'projeto02' title`, () => {
+  it(`should have the 'Projeto01' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto02');
+    expect(app.title).toEqual('Projeto01');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, projeto02');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Projeto01');
   });
 });
